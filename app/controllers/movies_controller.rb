0b0d506de6a2bde7a7669585d5e7490f4f4f2e59
julia817 <zhushejia0817@gmail.com>
2018-11-15 23:49:06 +0900

    fix some bugs on MARK buttons

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,25 +3,6 @@ class MoviesController < ApplicationController
 	def show
 		@movie = Movie.find(params[:id])
 		@movie_credits = Movie.credits params[:id]
-		check
 	end
-
-	private
-		def check
-			if logged_in?
-			user = current_user
-			watched_list = user.movielists.find_by(listname: "watched")
-			want_list = user.movielists.find_by(listname: "want")
-			rec_list = user.movielists.find_by(listname: "recommend")
-			unless watched_list.nil?
-				@watched_check = ListMovie.exists?(movielist_id: watched_list.id, movie_id:params[:id])
-			end
-			unless want_list.nil?
-				@want_check = ListMovie.exists?(movielist_id: want_list.id, movie_id:params[:id])
-			end
-			unless rec_list.nil?
-				@recommend_check = ListMovie.exists?(movielist_id: rec_list.id, movie_id:params[:id])
-			end
-		end
 
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,45 @@
 module MoviesHelper
+	def watched_check movie_id
+		# if logged_in?
+		# 	user = current_user
+		# 	watched_list = user.movielists.find_by(listname: "watched")
+		# 	unless watched_list.nil?
+		# 		ListMovie.exists?(movielist_id: watched_list.id, movie_id: movie_id)
+		# 	end
+		# end
+		check(movie_id, "watched")
+	end
+
+	def want_check movie_id
+		# if logged_in?
+		# 	user = current_user
+		# 	want_list = user.movielists.find_by(listname: "want")
+		# 	unless want_list.nil?
+		# 		ListMovie.exists?(movielist_id: want_list.id, movie_id: movie_id)
+		# 	end
+		# end
+		check(movie_id, "want")
+	end
+
+	def recommend_check movie_id
+		# if logged_in?
+		# 	user = current_user
+		# 	rec_list = user.movielists.find_by(listname: "recommend")
+		# 	unless rec_list.nil?
+		# 		ListMovie.exists?(movielist_id: rec_list.id, movie_id: movie_id)
+		# 	end
+		# end
+		check(movie_id, "recommend")
+	end
+
+	private
+		def check(movie_id, listname)
+			if logged_in?
+				user = current_user
+				@movielist = user.movielists.find_by(listname: listname)
+				unless @movielist.nil?
+					ListMovie.exists?(movielist_id: @movielist.id, movie_id: movie_id)
+				end
+			end
+		end
 end
