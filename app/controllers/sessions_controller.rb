b8669cfb965c4ac5f12c416f13be89d9ca86b470
julia817 <zhushejia0817@gmail.com>
2018-11-23 05:25:46 +0900

    sns login trial 4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,24 +4,23 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env['omniauth.auth']
-    raise env['omniauth.auth'].to_yaml
-    # if auth.present?
-    #   user = User.find_or_create_from_auth(request.env['omniauth.auth'])
-    #   session[:user_id] = user.id
-    #   redirect_back_or user
-    # else
-    # 	user = User.find_by(email: params[:session][:email].downcase)
-    # 	if user && user.authenticate(params[:session][:password])
-    # 		# log in & redirect to the show page
-    # 		log_in user
-    #     params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-    # 		redirect_back_or user
-    # 	else
-    # 		# error message
-    # 		flash.now[:danger] = 'メールアドレスとパスワードの組み合わせは有効ではありません'
-    # 		render 'new'
-    # 	end
-    # end
+    if auth.present?
+      user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+      session[:user_id] = user.id
+      redirect_back_or user
+    else
+    	user = User.find_by(email: params[:session][:email].downcase)
+    	if user && user.authenticate(params[:session][:password])
+    		# log in & redirect to the show page
+    		log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    		redirect_back_or user
+    	else
+    		# error message
+    		flash.now[:danger] = 'メールアドレスとパスワードの組み合わせは有効ではありません'
+    		render 'new'
+    	end
+    end
   end
 
   def destroy
