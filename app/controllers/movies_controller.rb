22c45b12ffeaaf4469fcd6c89d949e0225b7d606
Julia <zhushejia0817@gmail.com>
2019-02-20 11:31:18 +0900

    change search-people results layout & movie-info layout

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,16 +2,35 @@ class MoviesController < ApplicationController
   include MoviesHelper
 
   def show
-    # @movie = Movie.find(params[:id])
-    # @movie_credits = Movie.credits params[:id]
     @movie = Movie.details(params[:id])
-    if @movie["credits"]["cast"].count > 5
-      @cast_range = 5
+    if @movie["credits"]["cast"].count > 6
+      @cast_range = 6
     else
       @cast_range = @movie["credits"]["cast"].count
     end
     @trailers = Movie.trailers(params[:id])
-    # @collection = Movie.collection(@movie["belongs_to_collection"]["id"])
+
+    @directors = {}
+    @writers = {}
+    @story = {}
+    @screenplay = {}
+    @novel = {}
+    @music = {}
+    @movie["credits"]["crew"].each do |crew|
+      if crew["job"]=="Director" then @directors[crew["name"]] = crew["id"] end
+      if crew["job"]=="Writer" then @writers[crew["name"]] = crew["id"] end
+      if crew["job"]=="Story" then @story[crew["name"]] = crew["id"] end
+      if crew["job"]=="Screenplay" then @screenplay[crew["name"]] = crew["id"] end
+      if crew["job"]=="Novel" then @novel[crew["name"]] = crew["id"] end
+      if crew["job"]=="Original Music Composer" then @music[crew["name"]] = crew["id"] end
+    end
+    @directors_count = @directors.size
+    @writers_count = @writers.size
+    @story_count = @story.size
+    @screenplay_count = @screenplay.size
+    @novel_count = @novel.size
+    @music_count = @music.size
+
 
     # get current user's comment
     @current_user_comment = Comment.find_by(user_id: current_user.id, movie_id: @movie["id"]) if logged_in?
@@ -32,13 +51,13 @@ class MoviesController < ApplicationController
 
   def show_person
     @person = Movie.person_details(params[:id])
-    @person["also_known_as"].each do |n|
-      if !(n =~ /(?:\p{Hiragana}|\p{Katakana}|[一-龠々])/).nil?
-        @name = n
-        break
-      end
-    end
-    @name = @name.nil? ? @person["name"] : @name
+    # @person["also_known_as"].each do |n|
+    #   if !(n =~ /(?:\p{Hiragana}|\p{Katakana}|[一-龠々])/).nil?
+    #     @name = n
+    #     break
+    #   end
+    # end
+    # @name = @name.nil? ? @person["name"] : @name
     # @famous = ActiveSupport::JSON.decode(params[:known_for])
     @movie_credits = Array.new
     if @person["known_for_department"] == "Acting"
