a3980dad6a59d26d392be520dbb00e7eff25974d
Julia <zhushejia0817@gmail.com>
2019-03-13 18:15:36 +0900

    fix japanese titles problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  GENRES = { 'アクション' => 28, '冒険' => 12, 'コメディー' => 35, 'アニメーション' => 16, '犯罪' => 80, 'SF' => 878, 
-             'ミステリー' => 9648, 'ファンタシー' => 14, 'ロマンス' => 10749, 'ホラー' => 27, '音楽' => 10402, 'ファミリー' => 10751, 
-             'ドラマ' => 18, '歴史' => 36, '戦争' => 10752, 'スリラー' => 53, 'ドキュメンタリー' => 99, '西部劇' => 37 }
+  # GENRES = { 'アクション' => 28, '冒険' => 12, 'コメディー' => 35, 'アニメーション' => 16, '犯罪' => 80, 'SF' => 878, 
+  #            'ミステリー' => 9648, 'ファンタシー' => 14, 'ロマンス' => 10749, 'ホラー' => 27, '音楽' => 10402, 'ファミリー' => 10751, 
+  #            'ドラマ' => 18, '歴史' => 36, '戦争' => 10752, 'スリラー' => 53, 'ドキュメンタリー' => 99, '西部劇' => 37 }
 
   # confirm a logged-in user
   def logged_in_user
@@ -35,8 +35,9 @@ class ApplicationController < ActionController::Base
       movie["genres"].each do |genre|
         ids_arr << genre["id"]
       end
-      Movie.add(movie["id"], movie["title"], movie["poster_path"], ids_arr) unless ids_arr.nil?
-      Movie.add(movie["id"], movie["title"], movie["poster_path"], ids_str) unless ids_str.nil?
+      title = movie["original_language"]=="ja" ? movie["original_title"] : movie["title"]
+      Movie.add(movie["id"], title, movie["poster_path"], ids_arr) unless ids_arr.nil?
+      Movie.add(movie["id"], title, movie["poster_path"], ids_str) unless ids_str.nil?
     end
   end
 end
